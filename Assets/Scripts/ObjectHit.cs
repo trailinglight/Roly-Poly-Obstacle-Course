322fa17418014cb0797e767a75f1a564467bc10c
lighttrails <lighttrails@protonmail.com>
2021-12-10 07:50:28 -0700

    Updated scipt to create a color-blink coroutine for collision detection

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -4,8 +4,35 @@ using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
 {
+    float blinkTime = 0.5f;
+    MeshRenderer meshRenderer;
+    Color baseColor;
+
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        baseColor = meshRenderer.material.color;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
+    
         Debug.Log("Bumped into an obstacle!");
+
+        StartCoroutine(Blink());
+        
+    }
+
+    IEnumerator Blink()
+    {
+        //Color baseColor = GetComponent<MeshRenderer>().material.color;
+        meshRenderer.material.color = Color.cyan;
+
+        yield return new WaitForSeconds(blinkTime);
+
+        meshRenderer.material.color = baseColor;
+
+        yield return null;
     }
 }
